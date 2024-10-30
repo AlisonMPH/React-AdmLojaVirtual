7c904591262b508ef61f819dcbe42a20fd6fec9c
Alison <alisonmozer@gmail.com>
2024-10-30 14:15:14 -0300

    Aula 30102024

--- aula01/src/ProductForm.jsx
+++ aula01/src/ProductForm.jsx
@@ -1,12 +1,12 @@
+import FormInput from "./FormInput"
+
 const ProductForm = ({ handleChange, inputs, erros, isNew}) => {
   return (
     <>
         <div className="row">
-            <div className="form-floating">
-                <input type="text" className="form-control" id="productName" placeholder="" 
-                id="nome" name="nome" value={inputs?ProductForm.value} />
-                <label htmlFor="nome">Nome</label>
-                {erros?.nome && <div className="invalid-feedback">{erros?.nome}</div>}
+            <div className="col-12 mb-3">
+                <FormInput type="text" field="nome" label="Nome" value={inputs?.nome} 
+                onChange={handleChange} error={erros?.name} autofocus={true} />
             </div>
         </div>
     </>
