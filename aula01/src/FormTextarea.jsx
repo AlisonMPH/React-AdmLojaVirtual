7d9615bf70e6b8aa4b99b123f1350a9f5aff7683
Alison <alisonmozer@gmail.com>
2024-10-30 14:47:00 -0300

    Add Cleave

---
+++ aula01/src/FormTextarea.jsx
@@ -0,0 +1,15 @@
+const FormTextarea = ({ field, onChange, label, value, error, autofocus = false }) => {
+    return (
+        <>
+            <div className="form-floating">
+                <textarea className={`form-control $error ? 'is-invalid' : 'is-valid'}`}
+                    placeholder=" " id={field} name={field} onChange={onChange}
+                    autofocus={autofocus} style="height:3rem">value={value || ""}</textarea>
+                <label htmlFor={field}>{label}</label>
+            </div>
+            {error && <div className="invalid-feedback">{error}</div>}
+        </>
+    )
+}
+
+export default FormTextarea
--- aula01/src/ProductForm.jsx
+++ aula01/src/ProductForm.jsx
@@ -1,14 +1,27 @@
 import FormInput from "./FormInput"
+import FormTextarea from "./FormTextarea"
 
-const ProductForm = ({ handleChange, inputs, erros, isNew}) => {
+const ProductForm = ({ handleChange, inputs, erros, isNew }) => {
   return (
     <>
-        <div className="row">
-            <div className="col-12 mb-3">
-                <FormInput type="text" field="nome" label="Nome" value={inputs?.nome} 
-                onChange={handleChange} error={erros?.name} autofocus={true} />
-            </div>
+      <div className="row">
+        <div className="col-12 mb-3">
+          <FormInput type="text" field="nome" label="Nome" value={inputs?.nome}
+            onChange={handleChange} error={erros?.name} autofocus={true} />
         </div>
+        <div className="col-12 mb-3">
+          <FormInput type="number" field="preco" label="Preço" value={inputs?.preco}
+            onChange={handleChange} error={erros?.name} />
+        </div>
+        <div className="col-12 mb-3">
+          <FormTextarea field="descricao" label="Descrição" value={inputs?.descricao}
+            onChange={handleChange} error={erros?.descricao} />
+        </div>
+        <div className="col-12 mb-3">
+          <FormInput type="number" field="estoque" label="Estoque" value={inputs?.estoque}
+            onChange={handleChange} error={erros?.estoque} />
+        </div>
+      </div>
     </>
   )
 }
