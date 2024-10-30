fa70338daca0f5a88925b305b56470f9ac8a9487
Alison <alisonmozer@gmail.com>
2024-10-30 15:00:29 -0300

    Cleave

--- aula01/src/ProductForm.jsx
+++ aula01/src/ProductForm.jsx
@@ -1,3 +1,4 @@
+import CleaveInput from "./CleaveInput"
 import FormInput from "./FormInput"
 import FormTextarea from "./FormTextarea"
 
@@ -10,16 +11,21 @@ const ProductForm = ({ handleChange, inputs, erros, isNew }) => {
             onChange={handleChange} error={erros?.name} autofocus={true} />
         </div>
         <div className="col-12 mb-3">
-          <FormInput type="number" field="preco" label="Preço" value={inputs?.preco}
-            onChange={handleChange} error={erros?.name} />
+          <CleaveInput type="text" field="preco" label="Preço" value={inputs?.preco}
+            onChange={handleChange} error={erros?.preco}
+            options={{
+              prefix: 'R$', numeral: true, numeralThousandsGroupStyle: 'thousand',
+              rawValueTrimPrefix: true
+            }} />
         </div>
         <div className="col-12 mb-3">
           <FormTextarea field="descricao" label="Descrição" value={inputs?.descricao}
             onChange={handleChange} error={erros?.descricao} />
         </div>
         <div className="col-12 mb-3">
-          <FormInput type="number" field="estoque" label="Estoque" value={inputs?.estoque}
-            onChange={handleChange} error={erros?.estoque} />
+          <CleaveInput type="text" field="estoque" label="Estoque" value={inputs?.estoque}
+            onChange={handleChange} error={erros?.estoque}
+            options={{ numeral: true, numeralPositiveOnly: true, numeralDecimalScale: 0 }} />
         </div>
       </div>
     </>
